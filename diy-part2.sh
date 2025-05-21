e276d531a96b64ca0b9b5398cc082b1b16d2fd2c
zhuzhu888666 <167627499+zhuzhu888666@users.noreply.github.com>
2025-05-21 09:38:41 +0800

    Add files via upload

---
+++ diy-part2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+#添加root用户的密码，将密码为空改成password
+#sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# Modify default IP
+#修改登陆ip
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#x修改主题
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#修改名称
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+sed -i 's/ImmortalWrt/N60Pro/g' package/base-files/files/bin/config_generate
+
+#修改wifi名称（mtwifi-cfg）
+#sed -i 's/WHK_Telecom-2.4G/OpenWrt/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+#sed -i 's/WHK_Telecom/OpenWrt5G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+
+
+
+#加入rkp-ipid模块和turboacc
+ git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
+ curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
